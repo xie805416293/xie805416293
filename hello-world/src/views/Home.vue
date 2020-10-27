0ab7076259acabf17542e102458812bc4073451b
WIN-20200914KMU\谢仁丛 <805416293@qq.com>
2020-10-27 18:22:14 +0800

    暂存项目

---
+++ hello-world/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div> 
+        <p @click="gotoNext">{{text}}</p>
+        <label>点击</label>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            text: '谢仁丛'
+        }
+    },
+    methods: {
+        gotoNext() {
+            this.$router.push({
+                name: 'user',
+                params: {
+                    code: '1'
+                }
+            })
+        }
+    },
+}
+</script>
---
+++ hello-world/src/views/user.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="box">
+        <div class="box1"></div>
+        <div class="box2"></div>
+        <div class="box3"></div>
+    </div>
+</template>
+<script>
+export default {
+    created () {
+        const str = this.$route;
+        console.log(str);
+        const org = this.fun(1, 2);
+        console.log(org);
+        const a= {}
+        console.log(a);
+        console.log(window)
+    },
+    methods: {
+        fun(num1, num2) {
+            const num = num1 + num2;
+            if (num2 + 1 > 100) {
+                return num;
+            } else {
+                return this.fun(num, num2 + 1)
+            }
+        }
+    },
+}
+</script>
+<style scoped>
+.box{
+    width: 100%;
+    height: 100px;
+    display:flex;
+}
+.box1,.box3{
+    width: 100px;
+    height: 100px;
+    background-color: blue;
+}
+.box2{
+    flex: 1;
+    height: 100px;
+    background-color: green;
+}
+</style>
